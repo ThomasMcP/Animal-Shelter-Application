ff4743f33599bc60255832931d8fc2884d1763ba
Thomas McPhee <mcp.thom@gmail.com>
2018-11-09 16:10:15 +0000

    renamed folder 'project_photos' to 'project_planning_photos'; also created owner.rb class ended initialize function

---
+++ models/owner.rb
@@ -0,0 +1,17 @@
+require_relative('../db/sql_runner.rb')
+require_relative('./animal.rb')
+
+class Owner
+
+  attr_accessor :name, :address
+  attr_reader :id,
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @address = options['address']
+  end
+
+
+
+end
