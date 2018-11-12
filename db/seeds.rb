b527488e8dd6a6e1675bf23af54b16953d573b28
Thomas McPhee <mcp.thom@gmail.com>
2018-11-12 10:11:12 +0000

    added new animal and saved. Created and tested class function self.all

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,16 @@ animal1 = Animal.new({
   'day_admitted' => '2018-08-14',
   'age' => 2 })
 
+animal2 = Animal.new({
+  'name' => 'Christopher Eccelston',
+  'adoption_status' => 'Adoptable',
+  'day_admitted' => '2008-02-15',
+  'age' => 10 })
+
 
 animal1.save
+animal2.save
+
+Animal.all
+binding.pry
+nil
--- models/animal.rb
+++ models/animal.rb
@@ -14,6 +14,13 @@ class Animal
     @age = options['age']
   end
 
+  def self.all()
+    sql = "SELECT * FROM animals"
+    values = []
+    result = SqlRunner.run(sql, values)
+    return result.map { |animal| Animal.new(animal)}
+  end
+
   def save()
     sql = "INSERT INTO animals (name, adoption_status, day_admitted, age) VALUES ( $1, $2, $3, $4) RETURNING id;"
     values = [@name, @adoption_status, @day_admitted, @age]
