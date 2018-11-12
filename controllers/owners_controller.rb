1230eeb7ee48e292fd19145b8392efeba5deb482
Thomas McPhee <mcp.thom@gmail.com>
2018-11-12 16:11:46 +0000

    added missing reader to owner.rb, made REST link to all owners page.

---
+++ controllers/owners_controller.rb
@@ -0,0 +1,9 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/owner.rb')
+also_reload('./models/*')
+
+get '/owners' do
+  @owners = Owner.all
+  erb(:"owners/owners")
+end
