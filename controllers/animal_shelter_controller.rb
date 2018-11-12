fe584af995f20e6341299e50bcf0f8ca6e8e59ce
Thomas McPhee <mcp.thom@gmail.com>
2018-11-12 16:06:13 +0000

    added app.rb file. restructured requires for controllers.

--- controllers/animal_shelter_controller.rb
+++ controllers/animal_shelter_controller.rb
@@ -1,7 +1,8 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('models/animal.rb')
-require_relative('models/owner.rb')
+require_relative('../models/animal.rb')
+require_relative('../models/owner.rb')
+require_relative('./animals_controller.rb')
 also_reload('./models/*')
 
 get '/home' do
---
+++ controllers/animals_controller.rb
@@ -0,0 +1,14 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/animal.rb')
+also_reload('./models/*')
+
+get '/animals' do
+  @animals = Animal.all
+  erb(:"animals/animals")
+end
+
+get '/animals/new' do
+  @houses = Animal.all
+  erb(:new)
+end
