a3548176f92dd0206558cdcfc78d6d05e47ce1bd
Thomas McPhee <mcp.thom@gmail.com>
2018-11-13 13:14:59 +0000

    added new seeds. updated index and show files with bold tags.

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,11 +23,35 @@ animal2 = Animal.new({
   'age' => 10 })
 animal2.save
 
+animal3 = Animal.new({
+  'name' => 'Maisie',
+  'type' => 'Cat',
+  'breed' => 'Ginger One',
+  'adoption_status' => 'Not Adoptable',
+  'day_admitted' => '2004-02-15',
+  'age' => 14 })
+animal3.save
+
+animal4 = Animal.new({
+  'name' => 'Mr Ekkles',
+  'type' => 'Dog',
+  'breed' => 'Pug',
+  'adoption_status' => 'Not Adoptable',
+  'day_admitted' => '2015-02-15',
+  'age' => 5 })
+animal4.save
+
 owner1 = Owner.new({
   'name' => 'Johnny Teaspoons',
   'address' => '44 Wire Place, Edinburgh',
   'pet_id' => animal1.id })
 owner1.save
+
+owner2 = Owner.new({
+  'name' => 'Mr David Dickinson',
+  'address' => '1 Bronzed Place, Shropshire',
+  'pet_id' => animal2.id })
+owner2.save
 
 binding.pry
 nil
